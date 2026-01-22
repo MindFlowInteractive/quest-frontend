03a1a80c0e9182374f80b87067dd93aa0a11951b
Chiziterem Eze <chiziteremoluchi5@gmail.com>
2026-01-22 16:55:35 +0100

    feat(GameModes): enhance GameModeDescription and GameModesList components with improved structure and styling

--- src/components/GameMode/GameModeDescription.tsx
+++ src/components/GameMode/GameModeDescription.tsx
@@ -1,9 +1,96 @@
 import React from 'react'
 
+interface DescriptionList {
+    id: string;
+    description: string;
+    features: string[];
+    instructions: string[];
+}
+
+const descriptionLists: DescriptionList[] = [{
+    id: 'classic',
+    description: 'you to answer a series of questions in a timed format. Each question has four options, and players must select the correct one.',
+    features: [
+        'Standard scoring based on accuracy and time.',
+        'Leaderboards to encourage competition.'
+    ],
+    instructions : [
+        'Start the game and read the question.',
+        'Select one of the four options (A, B, C, or D).',
+        'You have 2 minutes per question. Make your choice before time runs out.',
+        'Your score is based on accuracy and speed',
+        'Answer the questions correctly within the time limit.'
+    ]
+}]
+
+const SubHeaderText = ({ children }: { children: string }) => <h3 className='text-lg lg:text-xl text-white font-medium'>
+   {children}
+</h3>
+
+const Container: React.FC<{ children: React.ReactNode }> = ({ children }: { children: React.ReactNode }) => (
+    <div className='border border-[#323336] rounded-sm p-4 xl:p-5 w-full text-left flex flex-col gap-5 leading-[100%] cursor-pointer'>
+        {children}
+    </div>
+)
+
+const Paragraph = ({ children }: { children: string }) => (
+    <p className='text-white text-base lg:text-lg font-light text-left'>
+        {children}
+    </p>
+)
+
+/* Vector 26 */
+
+/* Vector 26 */
+
+
+
 const GameModeDescription = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col gap-4 lg:gap-8'>
+
+
+        {descriptionLists.map((desc) => (
+        <>
+            <div className='relative text-xl font-light uppercase'>
+                <div className='w-full block shadow-[inset_3px_3px_24px_#033330] bg-[#02211F] h-14 lg:h-16 xl:h-18.25 mix-blend-color rounded-[11px] [clip-path:polygon(0%_0%,100%_0%,100%_73%,0%_100%)] -skew-x-20 text-white -top-10 xl:-top-20'></div>
+                <h2 className='absolute top-1/2 left-1/7 transform -translate-x-1/2 -translate-y-1/2 text-white text-base lg:text-lg font-light text-left'>
+                    {desc.id.replace(/-/g, ' ').toUpperCase()}
+                </h2>
+            </div>
+            <article key={desc.id}  className='w-[97%] lg:w-151.5 text-white font-light uppercase flex flex-col gap-4'>
+                <section>
+                    <Container>
+                        <SubHeaderText>Description</SubHeaderText>
+                        <Paragraph>{desc.description}</Paragraph>
+                    </Container>
+                </section>
+
+                <section>
+                    <Container>
+                        <SubHeaderText>Features</SubHeaderText>
+                        <ul className='list-disc list-inside text-white text-sm lg:text-base font-light'>
+                            {desc.features.map((feature, idx) => (
+                                <li key={idx}>{feature}</li>
+                            ))}
+                        </ul>
+                    </Container>
+                </section>
+
+                <section>
+                    <Container>
+                        <SubHeaderText>Instructions</SubHeaderText>
+                        <ul className='list-disc list-inside text-white text-sm lg:text-base font-light'>
+                            {desc.instructions.map((instruction, idx) => (
+                                <li key={idx}>{instruction}</li>
+                            ))}
+                        </ul>
+                    </Container>
+                </section>
+            </article>
+        </>
+
+        ))}
     </div>
   )
 }
--- src/components/GameMode/GameModesList.tsx
+++ src/components/GameMode/GameModesList.tsx
@@ -33,18 +33,18 @@ const gameModes: GameMode[] = [{
 const GameModesList = () => {
 
     const lists = gameModes.map((mode) => (
-        <li key={mode.id} className='border border-[#323336] rounded-sm p-3 active:bg-[#01100F] focus-within:bg-[#01100F] active:text-[#43AD6C] focus:text-[#43AD6C] hover:bg-[#01100F] hover:text-[#43AD6C] w-full text-center leading-[100%] '>
+        <li key={mode.id} className='border border-[#323336] rounded-sm p-2 xl:p-4 active:bg-[#01100F] focus-within:bg-[#01100F] active:text-[#43AD6C] focus:text-[#43AD6C] hover:bg-[#01100F] hover:text-[#43AD6C] w-full text-center leading-[100%] cursor-pointer'>
             {mode.name}
         </li>
     ))
     
   return (
-    <aside className='w-82 xl:w-100 2xl:w-122.5 flex flex-col gap-8 justify-center items-center m-3'>
+    <aside className='w-82 xl:w-100 2xl:w-122.5 flex flex-col gap-9 justify-center items-center m-3'>
         <figure>
             <img src={gameModeCategory} alt="Game Mode Categories" className='h-auto select-none pointer-events-none '/>
         </figure>
 
-        <ol className='flex flex-col gap-5 justify-center items-center w-full text-white uppercase text-2xl lg:text-3xl font-medium '>
+        <ol className='flex flex-col gap-5 lg:gap-7 justify-center items-center w-full text-white uppercase text-xl md:text-2xl xl:text-3xl font-medium '>
             {lists}
         </ol>
     </aside>
