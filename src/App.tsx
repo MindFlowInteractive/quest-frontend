706c5c1b0327707d99568d9b94e7dd296ad60305
emmanuel iheanacho <emmanueliheanacho04@gmail.com>
2026-01-22 17:58:28 +0100

    implemented responsive hero section for landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import FaqsSection from "./components/FaqsSection"
 import ContributorsSection from "./components/ContributorsSection"
+import HeroSection from "./components/HeroSection"
 function App() {
  
 
   return (
     <>
+      <HeroSection/>
       <ContributorsSection/>
       <FaqsSection/>
     </>
